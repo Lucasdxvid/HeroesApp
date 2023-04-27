424bbcc3f7ae8a2e2ce744b02334f1635f3ad3cd
lucasdxvid <athl12311@gmail.com>
2023-04-27 18:06:03 -0300

    search y error

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -14,6 +14,10 @@ export const SearchPage = () => {
   //! Helper para filtrar heroe acorde al "name"
   const heroes = getHeroesByName(q); // La Q es la query de arriba
 
+  //! Mensaje alterta (condicional)
+  const showSearch = q.length === 0; // Devuelve un booleano
+  const showError = q.length > 0 && heroes.length === 0; // Devuelve un booleano
+
   //! Mantener el valor - useForm (nuestro customHook)
   const { searchText, onInputChange } = useForm({
     searchText: q, // esto esta "" para que al recargar la pag se quede vacio pero si pones el mismo valor de q, entonces al recargar la pagina tendra el valor del query
@@ -22,7 +26,7 @@ export const SearchPage = () => {
   //! Prevent Default + formateo del texto en search
   const onSearchSubtmit = (e) => {
     e.preventDefault();
-    if (searchText.trim().length <= 1) return; //quitamos espacios y si es menor a 1 no devolvemos nada
+    //? if (searchText.trim().length <= 1) return; //quitamos espacios y si es menor a 1 no devolvemos nada
     navigate(`?q=${searchText}`); //Definimos la ruta acorde al valor de nuestro search
     console.log({ searchText });
   };
@@ -53,13 +57,18 @@ export const SearchPage = () => {
         <div className="col-7">
           <h4>Resultados</h4>
           <hr />
-
-          <div className="alert alert-primary">Héroe</div>
-
-          <div className="alert alert-danger">
+          <div
+            className="alert alert-primary animate__animated animate__fadeIn"
+            style={{ display: showSearch ? "" : "none" }}
+          >
+            Busca a tu héroe favorito
+          </div>
+          <div
+            className="alert alert-danger animate__animated animate__fadeIn"
+            style={{ display: showError ? "" : "none" }}
+          >
             No se encontró a <b className="pB">{q}</b>
           </div>
-
           {heroes.map((hero) => (
             <HeroCard key={hero.id} {...hero} /> // desestructuramos el resto de elementos
           ))}
