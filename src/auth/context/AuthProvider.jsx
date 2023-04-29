ea1dadfd341727078a7f132e72807d1850c0c2c9
lucasdxvid <athl12311@gmail.com>
2023-04-29 16:49:11 -0300

    localStorage de login

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -4,25 +4,37 @@ import { AuthContext } from "./AuthContext";
 import { authReducer } from "./authReducer"; //* Usamos el reducer que creamos como plantilla - parametro del useReducer
 import { types } from "../types/types";
 
-//! Estado inicial
-const initialState = {
-  logged: false,
+//! Estado inicial 
+//? Lo comentamos porque ahora el INIT se encarga de almacenar esas propiedades por lo que en vez de colocar initialState colocamos un objeto vacio {} en el useReducer
+// const initialState = {
+//   logged: false,
+// }; 
+
+//! Inicializador (3er argumento) - lo usamos para almacenar la info del user
+const init = () => {
+  const user = JSON.parse(localStorage.getItem("user")); // Si regresa null no regresa nada pero si regresa, nos da el user
+  return {
+    //* Siempre tiene que retornar algo el INIT (como el estado inicial)
+    logged: !!user, // Si el user existe !! entonces que almacene el usuario (!! convierte un booleano)
+    user: user,
+  };
 };
 
 //Recibe hijos (componentes hijos) - high order component
 export const AuthProvider = ({ children }) => {
   //! useReducer para controlar varios estados de la info que proveemos - usamos el reducer que creamos como plantilla
   // Desestructuramos el state (cualquier nombre y el dispatch) y recibe el reducer, initialState
-  const [authState, dispatch] = useReducer(authReducer, initialState);
+  const [authState, dispatch] = useReducer(authReducer, {}, init); //? Usaremos el 3er posible argumento el cual es el INIT (inicializador) para almacenar la info del login con localStorage
 
   const login = (name = "") => {
+    const user = { id: "Dual", name }; // definimos el usuario
+
     const action = {
       type: types.login, // Extraemos los types de nuestro js del login y logout
-      payload: {
-        id: "dual",
-        name: name,
-      },
+      payload: user, // la info adicional es el user
     };
+
+    localStorage.setItem("user", JSON.stringify(user)); //? colocamos key/clave "user" y segundo el valor en string (el user de objeto a string)
 
     dispatch(action); // despachamos la accion que recibimos de arriba
   };
